42ac89009b1b19d877eaa2c24a40a9d47384bd08
brian.orwe <brian.orwe@gmail.com>
2024-07-24 19:55:16 +0300

    Test for success

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,4 @@
-import dotenv from "dotenv"
-import assert from "assert"
-import { Validator } from "./core"
-
 async function main(){
-  dotenv.config()
-  const name = process.env.WORKNAME
-  const year = process.env.WORKYEAR
-  const id = process.env.WORKID
-  assert(name!=undefined)
-  assert(year!=undefined)
-  assert(id!=undefined)
-
-  const result = await new Validator(name, id, year,)
-    .begin().catch(reject=>{
-      console.log(reject)
-      return false
-    })
-  console.log("SUCCESS", result)
 }
 
 main()
--- tests/core.test.ts
+++ tests/core.test.ts
@@ -1,7 +1,23 @@
 import { describe, expect, it } from "@jest/globals"
+import dotenv from "dotenv"
+import { Validator } from "../src/core"
 
 describe("core tests",()=>{
-  it("test", ()=>{
-    expect(true).toBeFalsy()
-  })
+  dotenv.config()
+  const name = process.env.WORKNAME
+  const year = process.env.WORKYEAR
+  const id = process.env.WORKID
+  expect(name).toBeTruthy()
+  expect(year).toBeTruthy()
+  expect(id).toBeTruthy()
+
+  it("Validator User Correct", async ()=>{
+
+    const result = await new Validator(name!, id!, year!,)
+      .begin().catch(reject=>{
+        console.log(reject)
+        return false
+      })
+    expect(result).toBeTruthy()
+  }, 9999999)
 })
